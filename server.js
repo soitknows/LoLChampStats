841b3b6c6aef5a6c19718e993c7652899f81d8e7
Kyle Scully <scullykns@gmail.com>
2017-10-28 22:42:53 -0700

    added express server for serving page and content locally to aid in development

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+// serve the whole directroy as static files
+app.use(express.static(__dirname));
+
+// Start the app by listening on the default Heroku port or 8080
+app.listen(process.env.PORT || 8080);
+
+console.log('Your content is now served on localhost:8080');
+
+//route
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
